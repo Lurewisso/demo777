f36f15314ce28c5a9b90a3a6c7843ec545404c11
Pc-6 <githab.akk@gmail.com>
2025-06-25 20:25:46 +0300

    not main features

--- src/main/java/com/example/demo/entity/Role.java
+++ src/main/java/com/example/demo/entity/Role.java
@@ -14,7 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 @Builder
-@Table(schema = "users_schema", name = "t_credential")
+@Table(schema = "users_schema", name = "t_roles")
 
 public class Role {
     @Id
