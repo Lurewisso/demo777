02178e943f089fbf92e8f267d77f6edc4b5e1bdc
Pc-6 <githab.akk@gmail.com>
2025-06-25 20:45:31 +0300

    not main features

--- src/main/java/com/example/demo/entity/Credential.java
+++ src/main/java/com/example/demo/entity/Credential.java
@@ -1,13 +1,49 @@
 package com.example.demo.entity;
+//
+//
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import javax.management.relation.Role;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Entity
+//@Builder
+//@Table(schema = "users_schema", name = "t_credentials")
+//public class Credential {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    Long id;
+//
+//    @Column(nullable = false, name = "c_username")
+//    String username;
+//    @Column(nullable = false,name = "c_password")
+//    String password;
+//
+//    @ManyToOne()
+//    @JoinColumn(name = "c_role_id")
+//    Role role;
+//
+//    @OneToOne(cascade = CascadeType.ALL)
+//    @JoinColumn(name = "c_user_id",referencedColumnName = "id")
+//    User user;
+//
+//
+//}
 
+//package org.example.d.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.management.relation.Role;
 
 @Data
 @AllArgsConstructor
@@ -16,23 +52,19 @@ import javax.management.relation.Role;
 @Builder
 @Table(schema = "users_schema", name = "t_credentials")
 public class Credential {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
-
     @Column(nullable = false, name = "c_username")
     String username;
-    @Column(nullable = false,name = "c_password")
+    @Column(nullable = false, name = "c_password")
     String password;
 
-    @ManyToOne()
-    @JoinColumn(name = "c_role_id",referencedColumnName = "id")
+    @ManyToOne
+    @JoinColumn(name = "c_role_id")
     Role role;
 
     @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "c_user_id",referencedColumnName = "id")
+    @JoinColumn(name = "c_user_id", referencedColumnName = "id")
     User user;
-
-
 }
--- src/main/java/com/example/demo/entity/Role.java
+++ src/main/java/com/example/demo/entity/Role.java
@@ -1,6 +1,36 @@
 package com.example.demo.entity;
 
 
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import java.util.List;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Entity
+//@Builder
+//@Table(schema = "users_schema", name = "t_roles")
+//
+//public class Role {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    Long id;
+//
+//    @Column(nullable = false, name = "c_name")
+//    String name;
+//
+//    @OneToMany(mappedBy = "role")
+//    List<Credential> credentials;
+//
+//
+
+//package org.example.relations.entity;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,7 +45,6 @@ import java.util.List;
 @Entity
 @Builder
 @Table(schema = "users_schema", name = "t_roles")
-
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,9 +55,6 @@ public class Role {
 
     @OneToMany(mappedBy = "role")
     List<Credential> credentials;
-
-
-
-
-
 }
+
+
