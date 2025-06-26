1161ea0b49317c695ceecc8537025e44951d6963
Pc-6 <githab.akk@gmail.com>
2025-06-26 19:27:08 +0300

    not main featuress

--- src/main/java/com/example/demo/controllers/AuthController.java
+++ src/main/java/com/example/demo/controllers/AuthController.java
@@ -3,6 +3,9 @@ package com.example.demo.controllers;
 
 import com.example.demo.DTO.RegisterRequest;
 import com.example.demo.services.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.http.ResponseEntity;
@@ -11,9 +14,16 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
+
+@Tag(name = "Auth API", description = "auth account")
+
 public class AuthController {
     private final AuthService authService;
 
+
+    @Operation(summary = "Register", description = "Register now", responses = {
+            @ApiResponse(responseCode = "200",description = "Register successfully")
+    })
     @PostMapping("/register")
     public ResponseEntity<?> register (@RequestBody RegisterRequest registerRequest){
         authService.register(registerRequest);
@@ -21,6 +31,9 @@ public class AuthController {
 
     }
 
+    @Operation(summary = "Login", description = "Login", responses = {
+            @ApiResponse(responseCode = "200",description = "Register successfully")
+    })
     @GetMapping
     public ResponseEntity<?> login(@RequestBody Authentication authentication){
         return ResponseEntity.ok("Login was successful" + authentication.getName());
